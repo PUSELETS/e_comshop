e931ef23abcff1dc816975c3a76f123a65821ab5
unknown <dimamabolo15@gmail.com>
2023-10-20 11:18:46 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+import Home from './home';
+
+export const getAllAdminProducts = async () => {
+  try {
+    const res = await fetch("https://PUSELETS.github.io/e_comshop/api/admin/all-product", {
+      method: "GET",
+      cache: "no-store",
+    });
+
+    const data = await res.json();
+   
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function AdminAllProducts() {
+
+  const allAdminProducts = await getAllAdminProducts()
+
+  return <Home data={allAdminProducts && allAdminProducts.data} />
+}
