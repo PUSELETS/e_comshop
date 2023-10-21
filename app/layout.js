91358780d10801bce57dfedbd79fe377b6d3cbd8
PUSELETS <dimamabolo15@gmail.com>
2023-10-21 13:32:13 +0100

    meta

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />  
+        <meta name="viewport" content="viewport-fit=cover" />  
       </Head>
       <StateContext>
         <body className={inter.className}>
