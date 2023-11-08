a4a588ed53dfd08b0a08b26c360ae71cc863d19f
PUSELETS <dimamabolo15@gmail.com>
2023-11-08 03:54:06 +0000

    cxche

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Home from './home';
 
+export const fetchCache = 'force-no-store';
+
 export const getAllAdminProducts = async () => {
   try {
     const res = await fetch("https://e-comshop.vercel.app/api/admin/all-product", {
--- app/product/[details]/page.js
+++ app/product/[details]/page.js
@@ -1,5 +1,7 @@
 import ProductDetails from "@/components/ProductDetails";
 
+export const fetchCache = 'force-no-store';
+
 export const productById = async (id) => {
     try {
       const res = await fetch(
